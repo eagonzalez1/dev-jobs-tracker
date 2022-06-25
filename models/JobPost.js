6aa4c37894bb38df1069ccb7c13f01891ba82421
eagonzalez81 <gonzoatc81@gmail.com>
2022-06-25 19:35:44 -0400

    create jobPost model

---
+++ models/JobPost.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const contactsSchema = new mongoose.Schema({
+  name: {type: String, required: true, unique: true},
+  contacts: Boolean,
+  replied: Boolean
+}, {
+  timestamps: true
+})
+
+
+const jobPostSchema = new Schema({
+  companyName: String,
+  role: String,
+  sector: String,
+  salaryRange: Number,
+  motivation: {type: Number, min: 1, max: 5, default: 3},
+  link: String,
+  notes: String,
+  application: Boolean,
+  applied: Date,
+  appReply: Boolean,
+  contacts: [contactsSchema],
+  reqLanguagues: {type: Schema.Types.ObjectId, ref: "Languages"}
+}, {
+  timestamps: true
+})
+
+const JobPost = mongoose.model('JobPost', jobPostSchema)
+
+export {
+  JobPost
+}

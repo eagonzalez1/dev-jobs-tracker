1ba214b4f9954e163578ae87947d758ac0028a65
eagonzalez81 <gonzoatc81@gmail.com>
2022-07-01 13:41:31 -0400

    syntax correction

--- routes/jobPosts.js
+++ routes/jobPosts.js
@@ -1,38 +1,38 @@
 import { Router } from 'express'
-import * as jobPostCtrl from '../controllers/jobPosts.js'
+import * as jobPostsCtrl from '../controllers/jobPosts.js'
 import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
 // GET localhost:3000/jobPosts
-router.get('/', isLoggedIn, jobPostCtrl.index)
+router.get('/', isLoggedIn, jobPostsCtrl.index)
 
 // localhost:3000/jobPosts/new
-router.get('/new', isLoggedIn, jobPostCtrl.new)
+router.get('/new', isLoggedIn, jobPostsCtrl.new)
 
 // GET localhost:3000/jobPosts/:id/edit
-router.get('/:id/edit', isLoggedIn, jobPostCtrl.edit)
+router.get('/:id/edit', isLoggedIn, jobPostsCtrl.edit)
 
 // POST localhost:3000/jobPosts
-router.post('/', isLoggedIn, jobPostCtrl.create)
+router.post('/', isLoggedIn, jobPostsCtrl.create)
 
 // POST localhost:3000/jobPosts/:id/contacts
-router.post('/:id/contacts', isLoggedIn, jobPostCtrl.createContact)
+router.post('/:id/contacts', isLoggedIn, jobPostsCtrl.createContact)
 
 // POST localhost:3000/jobPosts/:id/contacts
-router.post('/:id/reqLanguages', isLoggedIn, jobPostCtrl.addLanguage)
+router.post('/:id/reqLanguages', isLoggedIn, jobPostsCtrl.addLanguage)
 
 // DELETE -- localhost:3000/:id
-router.delete('/:id', jobPostCtrl.delete)
+router.delete('/:id', jobPostsCtrl.delete)
 
 // PUT -- localhost:3000/jobPosts/:id
-router.put('/:id', jobPostCtrl.update)
+router.put('/:id', jobPostsCtrl.update)
 
 // DELETE -- localhost:3000/:id
-router.delete('/:id/contacts/:contactId', jobPostCtrl.deleteContact)
+router.delete('/:id/contacts/:contactId', jobPostsCtrl.deleteContact)
 
 // DELETE -- localhost:3000/:id
-router.delete('/:id/reqLanguages/:languageId', jobPostCtrl.deleteReqLanguage)
+router.delete('/:id/reqLanguages/:languageId', jobPostsCtrl.deleteReqLanguage)
 
 export {
   router
